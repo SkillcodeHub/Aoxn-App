f62d82337e1d9a4f46b4546d7349f84e0b469928
Parth Patel <patelparth292906@gmail.com>
2023-06-28 13:06:15 +0530

    changes 28 01

--- lib/res/components/appbar/payment_widget.dart
+++ lib/res/components/appbar/payment_widget.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 import '../../../View/PaymentHistory/payment_history_screen.dart';
+import '../../../view/PaymentHistory/payment_history_screen.dart';
 
 class PaymentWidget extends StatelessWidget {
   const PaymentWidget({
     super.key,
   });
-
   @override
   Widget build(BuildContext context) {
     return InkWell(
